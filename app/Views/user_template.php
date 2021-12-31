bdda4de6b05b13c558bc3bfbbb464881a9b83ca7
Fara27 <fara27days@yahoo.com>
2021-12-31 19:59:40 +0100

    material design bootstrap

--- app/Views/user_template.php
+++ app/Views/user_template.php
@@ -9,11 +9,12 @@
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
     <!-- Bootstrap core CSS -->
-    <link href="<?php echo base_url();?>/app/public/admin/assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?php echo base_url();?>/assets/css/bootstrap.min.css" rel="stylesheet">
+    
     <!-- Material Design Bootstrap -->
-    <link href="<?php echo base_url();?>/app/public/admin/assets/css/mdb.min.css" rel="stylesheet">
+    <link href="<?php echo base_url();?>/assets/css/mdb.min.css" rel="stylesheet">
     <!-- Your custom styles (optional) -->
-    <link href="<?php echo base_url();?>/app/public/admin/assets/css/style.min.css" rel="stylesheet">
+    <link href="<?php echo base_url();?>/assets/css/style.min.css" rel="stylesheet">
     <style type="text/css">
 
       html,
